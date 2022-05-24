03e63245980027b4cd82975e43b0472b6d49ed7f
Mario Espejo <mespluc@gmail.com>
2022-05-25 01:33:04 +0200

    Added all files to new repository curso-springboot

---
+++ src/main/java/com/cursojava/curso/models/Usuario.java
@@ -0,0 +1,37 @@
+package com.cursojava.curso.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+@ToString
+public class Usuario {
+
+    @Id
+    @Getter @Setter @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter @Setter @Column(name = "nombre")
+    private String nombre;
+
+    @Getter @Setter @Column(name = "apellido")
+    private String apellido;
+
+    @Getter @Setter @Column(name = "email")
+    private String email;
+
+    @Getter @Setter @Column(name = "telefono")
+    private String telefono;
+
+    @Getter @Setter @Column(name = "password")
+    private String password;
+
+
+
+
+}
